0546a760ad2adc5d88c129d2e8e6551382a575c0
Prabal <prabal19999@gmail.com>
2023-04-29 14:29:44 +0530

    read all the posts from firebase

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,30 +2,19 @@ import { SparklesIcon } from '@heroicons/react/outline'
 import React from 'react'
 import Input from './Input'
 import Post from './Post'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from '../../firebase'
 
 export default function Feed() {
-  const posts=[
-    {
-      id:"1",
-      name:"prabal",
-      username:"rationalprabal",
-       userImg:"https://avatars.githubusercontent.com/u/108731705?v=4",
-       img:"https://images.unsplash.com/photo-1680875894705-1b23d0bfd064?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-       text:"Nice one!",
-       timestamp:'2 hours ago'
+ const [posts,setPosts] =React.useState([])
 
-    },
-    {
-      id:"2",
-      name:"prabal",
-      username:"rationalprabal",
-       userImg:"https://avatars.githubusercontent.com/u/108731705?v=4",
-       img:"https://images.unsplash.com/photo-1681037690188-33ff11b9a05f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-       text:"3D Render!",
-       timestamp:'1 hours ago'
+ React.useEffect(()=>
+onSnapshot(query(collection(db,"posts"),orderBy("timestamp","desc")),(snapshot)=>{
+  setPosts(snapshot.docs)
+})
 
-    }
-  ]
+ ,[])
+
   return (
     <div className='xl:ml-[370px] sm:ml-[70px] w-full  sm:w-1/2 border-2'>
         <div className='flex py-2 px-3 sticky justify-between top-0 bg-white z-50 items-center'>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,23 +1,27 @@
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from '@heroicons/react/outline'
+
 import React from 'react'
+import Moment from 'react-moment'
 
 export default function Post({post}) {
   return (
     <div className='flex p-3  cursor-pointer border-b border-gray-200'>
-        <img className=  'w-11 h-11 border-6 cursor-pointer hover:brightness-95 rounded-full mr-4' src={post.userImg} alt="user Image" />
+        <img className=  'w-11 h-11 border-6 cursor-pointer hover:brightness-95 rounded-full mr-4' src={post.data().userImage} alt="user Image" />
         <div>
             <div className='flex items-center justify-between'>
                 <div className='flex space-x-1 whitespace-nowrap items-center'>
-                    <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post.name}</h4>
-                    <span className='text-sm sm:text-[15px]'>@{post.username}</span>
-                    <span className='text-sm sm:text-[15px] hover:underline'>{post.timestamp}</span>
+                    <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post.data().name}</h4>
+                    <span className='text-sm sm:text-[15px]'>@{post.data().username};</span>
+                    <span className='text-sm sm:text-[15px] hover:underline'>{
+                        <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
+                    }</span>
                 </div>
                 <div>
                     <DotsHorizontalIcon className='h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2'/>
                 </div>
                 </div>
-                <p className='text-gray-800 text-[15px] sm:text-[16px] mb-2'>{post.text}</p>
-                <img className='rounded-2xl mr-2' src={post.img}/>
+                <p className='text-gray-800 text-[15px] sm:text-[16px] mb-2'>{post.data().text}</p>
+                <img className='rounded-2xl mr-2' src={post.data().image}/>
                 <div className='flex justify-between text-gray-500 p-2'>
                     <ChatIcon className='h-9 hoverEffect w-9 p-2 hover:text-sky-500 hover:bg-sky-100'/>
                     <TrashIcon className='h-9 hoverEffect w-9 p-2 hover:text-red-600 hover:bg-red-100'/>

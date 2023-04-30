30e7b73f6693a755e08bf8dbc675799cc061ee80
Prabal <prabal19999@gmail.com>
2023-04-30 15:26:58 +0530

    added like functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,41 @@
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from '@heroicons/react/outline'
-
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore'
+import { signIn, useSession } from 'next-auth/react'
 import React from 'react'
 import Moment from 'react-moment'
+import { db } from '../../firebase'
 
 export default function Post({post}) {
+    const [likes,setLikes]= React.useState([])
+    const [hasLiked,setHasLiked]= React.useState(false)
+const {data:session}= useSession()
+    const likePost=async()=>{
+
+    if(session){
+        if(hasLiked){
+       
+            await deleteDoc(doc(db,"posts",post.id,"likes",session?.user.uid))
+                    }
+               else await setDoc(doc(db,"posts",post.id,"likes",session?.user.uid),{
+                username:session.user.username
+            })
+    }
+    else{
+        signIn()
+    }
+    }
+//! setting the likes count
+    React.useEffect(()=>{
+onSnapshot(collection(db,"posts",post.id,"likes"),(snapshot)=>setLikes(snapshot.docs))
+    },[db])
+
+    //! setting the like for the particular user
+
+    React.useEffect(()=>{
+setHasLiked(likes.findIndex(like=>like.id===session.user.uid)!==-1) //! if uid is there in the like array then it is gonna give us its index which can never be -1, so its gonna be true. if uid is not there in the like array then it is gonna give -1 and return false. So this is how we are setting the like as true or false.
+    },[likes])
+
   return (
     <div className='flex p-3  cursor-pointer border-b border-gray-200'>
         <img className=  'w-11 h-11 border-6 cursor-pointer hover:brightness-95 rounded-full mr-4' src={post.data().userImage} alt="user Image" />
@@ -25,7 +57,17 @@ export default function Post({post}) {
                 <div className='flex justify-between text-gray-500 p-2'>
                     <ChatIcon className='h-9 hoverEffect w-9 p-2 hover:text-sky-500 hover:bg-sky-100'/>
                     <TrashIcon className='h-9 hoverEffect w-9 p-2 hover:text-red-600 hover:bg-red-100'/>
-                    <HeartIcon className='h-9 hoverEffect w-9 p-2 hover:text-red-600 hover:bg-red-100'/>
+               <div className='flex items-center'>
+               {
+                        hasLiked ? <HeartIconFilled onClick={likePost} className='h-9 hoverEffect w-9 p-2 text-red-600 hover:bg-red-100'/> :     <HeartIcon onClick={likePost} className='h-9 hoverEffect w-9 p-2 hover:text-red-600 hover:bg-red-100'/>
+                    }
+                    {
+                        likes.length>0&&(
+                            <span className={`${hasLiked && "text-red-600"} text-sm` }>{likes.length}</span>
+                        )
+                    }
+               </div>
+                
                     <ShareIcon className='h-9 hoverEffect w-9 p-2 hover:text-sky-500 hover:bg-sky-100'/>
                     <ChartBarIcon className='h-9 hoverEffect w-9 p-2 hover:text-sky-500 hover:bg-sky-100'/>
 
